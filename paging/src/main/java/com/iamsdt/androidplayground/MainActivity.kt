a52a7285b8a3c87d299e409821a76c363d3fd823
Iamsdt <shudiptotrafder@gmail.com>
2018-06-03 17:08:33 +0600

    Change androidx to Android

--- paging/src/main/java/com/iamsdt/androidplayground/MainActivity.kt
+++ paging/src/main/java/com/iamsdt/androidplayground/MainActivity.kt
@@ -1,46 +1,15 @@
 package com.iamsdt.androidplayground
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.iamsdt.androidplayground.retrofit.MyDatabase
-import com.iamsdt.androidplayground.retrofit.RetDao
-import com.iamsdt.androidplayground.retrofit.RetDemo
-import com.iamsdt.androidplayground.retrofit.RetrofitInterface
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     //https://jsonplaceholder.typicode.com/photos
 
-    lateinit var retDao: RetDao
-    lateinit var myAdapter: MyAdapter
-    lateinit var retInterface: RetrofitInterface
-
-    val viewModel:MyViewModel by lazy {
-        ViewModelProviders.of(this).get(MyViewModel::class.java)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        retInterface = RetDemo().restInt!!
-
-        myAdapter = MyAdapter()
-
-        retDao = MyDatabase.getDatabase(this).bookDao
-
-        recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = myAdapter
-
-        viewModel.getData(retDao,retInterface).observe(this, Observer {
-            if (it != null && it.isNotEmpty()){
-                myAdapter.submitList(it)
-            }
-        })
     }
 }
